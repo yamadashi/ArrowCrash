a2ff8dc198b3e059a995e2bf74c9cfdac410f2d4
yamadashi <usaming27@gmail.com>
2018-10-08 01:52:44 +0900

    一応バグとしては解決したけど、根本的な原因がわからない

--- ArrowCrash/Scripts/Game/Player.cpp
+++ ArrowCrash/Scripts/Game/Player.cpp
@@ -3,9 +3,9 @@
 Player::Player(int player_num, const GameData& gameData_)
 	:number(player_num),
 	gameData(gameData_),
-	arrowBlocks(),
+	arrowBlocks(new std::vector<std::weak_ptr<ArrowBlock>>()),
 	field(new Field(gameData.stdPositions.at(number), gameData.blockSize)),
-	mngr(*field, arrowBlocks, gameData.stdPositions.at(number), gameData.blockSize)
+	mngr(*field, *arrowBlocks, gameData.stdPositions.at(number), gameData.blockSize)
 {}
 
 void Player::update() {
@@ -26,16 +26,16 @@ void Player::update() {
 void Player::draw() const {
 	field->draw();
 	mngr.draw();
-	PutText(arrowBlocks.size()).from(gameData.stdPositions[number]);
+	PutText(arrowBlocks->size()).from(gameData.stdPositions[number]);
 }
 
 void Player::explode() {
-	for (auto arrow : arrowBlocks) {
+	for (auto arrow : *arrowBlocks) {
 		const auto& ptr = arrow.lock();
 		if (ptr->isSettled())
 			field->explode(ptr->getPoint(), ptr->getDirection());
 	}
 	//‚¿‚å‚Á‚Æ‹C‚¿ˆ«‚¢
-	auto&& itr = std::remove_if(arrowBlocks.begin(), arrowBlocks.end(), [](const std::weak_ptr<ArrowBlock>& ref) { return ref.lock()->isDestroyed(); });
-	arrowBlocks.erase(itr, arrowBlocks.end());
+	auto&& itr = std::remove_if(arrowBlocks->begin(), arrowBlocks->end(), [](const std::weak_ptr<ArrowBlock>& ref) { return ref.lock()->isDestroyed(); });
+	arrowBlocks->erase(itr, arrowBlocks->end());
 }
--- ArrowCrash/Scripts/Game/Player.h
+++ ArrowCrash/Scripts/Game/Player.h
@@ -7,7 +7,7 @@ private:
 	const GameData& gameData;
 
 	// 一括で爆発するためフィールド上の全てのArrowBlockへの参照を持っておく
-	std::vector<std::weak_ptr<ArrowBlock>> arrowBlocks;
+	std::shared_ptr<std::vector<std::weak_ptr<ArrowBlock>>> arrowBlocks;
 	std::shared_ptr<Field> field;
 	BlockUnitManager mngr;
 
